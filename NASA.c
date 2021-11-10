73980ffe88f17c51fad47b34e9035a82daaa2e2a
MarkRustamov <i.r.mark14@gmail.com>
2021-11-10 22:15:45 +0300

    Update NASA.c

--- NASA.c
+++ NASA.c
@@ -6,9 +6,9 @@ int main()
     FILE *fin, *fout;
     fin = fopen("access_log_Jul95.txt", "r");
     fout = fopen("Answers.txt", "w");
-    char string[400], max_requestdate[2], const_date[2];
+    char string[400], max_requesttime[27], const_time[6];
     char *check;
-    int error_counter = 0, max_requests = -1, date_requests = 0;
+    int error_counter = 0, max_requests = -1, time_requests = 0;
     while (1){
         check = fgets (string, sizeof(string), fin);
         if (check == NULL){
@@ -31,36 +31,39 @@ int main()
             fprintf(fout, "\n");
         }
         i = 0;
-        char date[2];
-        while (string[i] != '['){
+        char time[6];
+        while (string[i] != ']'){
             i++;
         }
-        date[0] = string[i+1];
-        date[1] = string[i+2];
-        //fprintf(fout, "%c%c", date[0], date[1]);
-        if ((strcmp(const_date, date)) == 0){
-            date_requests++;
+        time[0] = string[i-11];
+        time[1] = string[i-10];
+        time[2] = string[i-9];
+        time[3] = string[i-8];
+        time[4] = string[i-7];
+        time[5] = '\0';
+        if (strncmp(time, const_time, 5) == 0){
+            time_requests++;
         }
         else{
-            const_date[0] = date[0];
-            const_date[1] = date[1];
-            if (date_requests > max_requests){
-                max_requests = date_requests;
-                max_requestdate[0] = date[0];
-                max_requestdate[1] = date[1];
+            strcpy(const_time, time);
+            if (time_requests > max_requests){
+                max_requests = time_requests;
+                i--;
+                int j = 25;
+                max_requesttime[26] = '\0';
+                while (string[i] != '['){
+                    max_requesttime[j] = string[i];
+                    j--;
+                    i--;
+                }
             }
-            date_requests = 0;
+            time_requests = 0;
         }
-        //fprintf(fout, "%c", lastspace_position+2);
     }
-    /*unsigned int counter = 0;
-    while ((check = fgets (string, sizeof(string), fin)) != NULL){
-        fprintf(fout, '%s', string);
-
-    }*/
-    fprintf(fout, "\n");
+    fprintf(fout, "<><><><><><><><><><><><><><><><>\n");
     fprintf(fout, "Number of errors is %d\n", error_counter);
-    fprintf(fout, "%c%c/Jul/1995  %d", max_requestdate[0], max_requestdate[1], max_requests);
+    fprintf(fout, "<><><><><><><><><><><><><><><><>\n");
+    fprintf(fout, "Max request time is %s\nMax number of requests is %d", max_requesttime, max_requests-1);
     fclose(fin);
     fclose(fout);
     return 0;
